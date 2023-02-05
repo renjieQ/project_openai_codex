b3c2d57ec112644b65edbf345fdfc66df63dc2b8
renjieQ <qurenjie@qq.com>
2023-02-05 19:02:09 +0800

    I finally get my own server in jp

--- client/script.js
+++ client/script.js
@@ -84,10 +84,10 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  // fetch data from local server -> bot's response
-  // const response = await fetch('http://localhost:5000/', {
-
-  const response = await fetch('https://codex-m4h6.onrender.com/', {
+  // fetch data from server -> bot's response
+  const response = await fetch('http://34.84.203.4:5000/', {
+    // const response = await fetch('http://localhost:5000/', {
+    // const response = await fetch('https://codex-m4h6.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
