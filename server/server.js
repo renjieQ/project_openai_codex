2fb00efcbc03c99109a02da40c245a5422e35a86
renjieQ <qurenjie@qq.com>
2023-03-07 18:26:00 +0800

    test

--- server/server.js
+++ server/server.js
@@ -48,11 +48,15 @@ app.post('/', async (req, res) => {
     //   presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     // });
 
-    const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: 'Hello world' }],
-    });
-    console.log(response.data.choices[0].message);
+    // const { Configuration, OpenAIApi } = require('openai');
+    const chapGPT = async (prompt) => {
+      const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: prompt }],
+      });
+      console.log(response['data']['choices'][0]['message']['content']);
+    };
+    chapGPT('what are some theories on what is one piece?');
 
     res.status(200).send({
       // bot: response.data.choices[0].text,
