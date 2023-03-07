2c8bc4a63ebe604517eac1e578d6d56bf0f73fe6
renjieQ <qurenjie@qq.com>
2023-03-07 19:11:35 +0800

    fix bug6+test

--- server/server.js
+++ server/server.js
@@ -35,20 +35,28 @@ app.post('/', async (req, res) => {
     //   presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     // });
 
-    const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        { role: 'system', content: 'You are a helpful and polite assistant.' },
-        { role: 'user', content: `${prompt}` },
-      ],
-      // temperature: 0.7, // Higher values means the model will take more risks.
-      // max_tokens: 4098, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-    });
-    console.log(response['data']['choices'][0]['message']['content']);
-
+    let aaa = '';
+    const chapGPT = async (prompt) => {
+      const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'system',
+            content: 'You are a helpful and polite assistant.',
+          },
+          { role: 'user', content: `${prompt}` },
+        ],
+        // temperature: 0.7, // Higher values means the model will take more risks.
+        // max_tokens: 4098, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      });
+      console.log(response['data']['choices'][0]['message']['content']);
+      aa = response['data']['choices'][0]['message']['content'];
+      return aa;
+    };
+    chapGPT(`${prompt}`);
     res.status(200).send({
       // bot: response.data.choices[0].text,
-      bot: response.data.choices[0].message,
+      bot: aa,
     });
   } catch (error) {
     console.error(error);
